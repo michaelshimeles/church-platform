990222a283ae7ada5b8db5e4a4c3c0eb4a5b675b
Michael Shimeles <michaelwasihun96@gmail.com>
2023-09-28 11:11:07 -0400

    Working on blog post

--- app/resources/blog/[slug]/page.tsx
+++ app/resources/blog/[slug]/page.tsx
@@ -1,11 +1,93 @@
-/**
- * v0 by Vercel Labs.
- * @see https://v0.dev/t/AfXYpLG
- */
-import Link from "next/link"
+"use client"
+import { client } from "@/sanity/lib/client"
+import { useEffect, useState } from "react"
 import Image from "next/image"
-export default function Blog() {
+import { urlForImage } from "@/sanity/lib/image"
+import { PortableText } from '@portabletext/react';
+import { Loader2 } from 'lucide-react';
+import { Skeleton } from "@/components/ui/skeleton"
+
+export const Icons = {
+    spinner: Loader2,
+};
+
+const SlugPage = ({ params }: { params: { slug: string } }) => {
+    const { slug } = params
+
+    const [post, setPost] = useState<any>(null)
+
+    const PortableTextComponent = {
+        types: {
+            image: ({ value }: { value: any }) => (
+                <Image src={urlForImage(value).url()} alt='' />
+            )
+        },
+        block: {
+            h1: ({ children }: any) => <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>,
+            h2: ({ children }: any) => <h2 className="pt-[1rem] pb-[1rem] scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0">{children}</h2>,
+            h3: ({ children }: any) => <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>,
+            h4: ({ children }: any) => <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>,
+            p: ({ children }: any) => <p className="pb-9 leading-7 [&:not(:first-child)]:mt-6">{children}</p>,
+            blockquote: ({ children }: any) => <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>,
+        },
+        list: {
+            bullet: ({ children }: any) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>,
+            number: ({ children }: any) => <ol>{children}</ol>,
+            checkmarks: ({ children }: any) => <ol>{children}</ol>,
+        },
+        listItem: {
+            bullet: ({ children }: any) => <li>{children}</li>,
+            number: ({ children }: any) => <li>{children}</li>,
+            checkmarks: ({ children }: any) => <li>✅ {children}</li>,
+        },
+
+    }
+    useEffect(() => {
+        async function getData(slug: string) {
+            const query = `*[_type == "post" && slug.current == "${slug}"][0] {
+                title,
+                  subtitle,
+               author->,
+                  body,
+                  slug,
+                  "image":mainImage.asset->,
+                  "authorImage": author->.image.asset->,
+                  "categories":categories[]->
+              }`;
+
+            const data = await client.fetch(query)
+
+            setPost(data)
+        }
+        getData(slug)
+    }, [])
+
+    if (!post) {
+        return (
+            <div className="flex justify-center items-center space-x-4 min-h-screen min-w-full">
+                <Skeleton className="h-12 w-12 rounded-full" />
+                <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                </div>
+            </div>
+        )
+    }
     return (
-        <></>
+        <div className="w-full pt-[5rem] px-[3rem] pb-[3rem]">
+            <div className="flex flex-col items-center min-w-fit">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl text-center">{post?.title}</h1>
+                <div className="flex items-center border border-slate-700 p-2 w-[11rem] gap-1 rounded-full mt-[1rem]">
+                    <img src={post?.authorImage?.url} alt={post?.author?.name} className="w-8 h-8 rounded-full" />
+                    <p className="text-center font-semibold text-sm">{post?.author?.name}</p>
+                </div>
+                <div className="w-[97%] md:w-[95%] lg:w-[90%] xl:w-[70%] mt-4">
+                    <PortableText value={post?.body} components={PortableTextComponent} />
+                </div>
+            </div>
+        </div>
+
     )
 }
+
+export default SlugPage
