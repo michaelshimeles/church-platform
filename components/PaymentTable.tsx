0d9e29fafab5b8c0af6a0ef62401a162c7766f3f
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-04 17:54:14 -0400

    Connected payments api

--- components/PaymentTable.tsx
+++ components/PaymentTable.tsx
@@ -109,7 +109,7 @@ export default function PaymentTable() {
                                             <DialogHeader>
                                                 <DialogTitle>Donation Info</DialogTitle>
                                                 <DialogDescription>
-                                                    Make changes to your profile here. Click save when you're done.
+                                                    Make changes to your profile here. Click save when you&apos;re done.
                                                 </DialogDescription>
                                             </DialogHeader>
                                             <div className="grid gap-2 py-4">
