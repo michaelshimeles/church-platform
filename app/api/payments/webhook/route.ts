e58b1e948c53c822ec7f031cf6606ef9f806c06b
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-08 11:50:15 -0400

    Making blog open for everyone

--- app/api/payments/webhook/route.ts
+++ app/api/payments/webhook/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const res = await req.json();
 
+  console.log("Res", res);
   const dateTime = new Date(res?.created * 1000).toLocaleDateString();
   const timeString = new Date(res?.created * 1000).toLocaleTimeString();
 
