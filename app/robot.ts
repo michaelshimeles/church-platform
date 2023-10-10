5eaad4e846d1e07c54a6a42f81e990f63b76d216
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-10 18:52:15 -0400

    SEO stuff pt4

--- app/robot.ts
+++ app/robot.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: ['/give', '/donations', "/profile", '/thank-you', '/user-profile'],
     },
     sitemap: 'https://tsegabiblechurch.com/sitemap.xml',
   }
