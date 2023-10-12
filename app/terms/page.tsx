f9fca9d495115e06b0691221c6872b3cb2947c9d
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-11 20:48:26 -0400

    add terms

---
+++ app/terms/page.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const ChurchTermsOfService = () => {
+    return (
+        <div className='flex justify-center p-5 min-w-screen'>
+            <div className='flex flex-col justify-center w-[70%] mt-5'>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Terms of Service
+                </h1>
+
+                <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    1. Terms
+                </h2>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    By accessing the website at <a href="https://www.tsegabiblechurch.com">www.tsegabiblechurch.com</a>, you are agreeing to be bound by these terms of service, all applicable laws, and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained on this website are protected by applicable copyright and trademark law.
+                </p>
+
+                <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    2. Use License
+                </h2>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    Permission is granted to temporarily download one copy of the materials (information or software) on Tsega Bible Fellowship Church&apos;s website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license, you may not:
+                    - modify or copy the materials;
+                    - use the materials for any commercial purpose or for any public display (commercial or non-commercial);
+                    - attempt to decompile or reverse engineer any software contained on Tsega Bible Fellowship Church&apos;s website;
+                    - remove any copyright or other proprietary notations from the materials; or
+                    - transfer the materials to another person or "mirror" the materials on any other server.
+                </p>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    This license shall automatically terminate if you violate any of these restrictions and may be terminated by Tsega Bible Fellowship Church at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession, whether in electronic or printed format.
+                </p>
+
+                <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    3. Disclaimer
+                </h2>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    The materials on Tsega Bible Fellowship Church&apos;s website are provided on an &apos;as is&apos; basis. Tsega Bible Fellowship Church makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
+                </p>
+
+                <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    4. Limitations
+                </h2>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    In no event shall Tsega Bible Fellowship Church or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Tsega Bible Fellowship Church&apos;s website, even if Tsega Bible Fellowship Church or a Tsega Bible Fellowship Church authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+                </p>
+
+                <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    5. Revisions and Errata
+                </h2>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    The materials appearing on Tsega Bible Fellowship Church&apos;s website could include technical, typographical, or photographic errors. Tsega Bible Fellowship Church does not warrant that any of the materials on its website are accurate, complete, or current. Tsega Bible Fellowship Church may make changes to the materials contained on its website at any time without notice. Tsega Bible Fellowship Church does not, however, make any commitment to update the materials.
+                </p>
+
+                <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    6. Governing Law
+                </h2>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    These terms and conditions are governed by and construed in accordance with the laws of [Your State/Country] and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.
+                </p>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    Your use of this website signifies your acceptance of these terms of service. If you do not agree to these terms, please do not use our website.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default ChurchTermsOfService;
