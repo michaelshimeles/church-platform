465902aede9f8d1294796c3b7fcf52bc0feae882
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-30 17:34:19 -0400

    Need to fix donations

--- app/(finance)/donations/page.tsx
+++ app/(finance)/donations/page.tsx
@@ -1,12 +1,23 @@
 import PaymentTable from '@/components/PaymentTable';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react'
 
-interface DonationProps {}
+interface DonationProps { }
 
 const Donations: React.FC<DonationProps> = ({ }) => {
     return (
-        <section className="container px-4 mx-auto min-h-screen py-[3rem]">
-            <PaymentTable />
+        <section className="flex flex-col gap-3 min-w-full justify-center items-center min-h-[70vh]">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Donations page is getting a revamp
+            </h1>
+            <p className="leading-7 [&:not(:first-child)]:mt-1">
+                If you are looking for your donation receipts, please check your email
+            </p>
+            <Link href="/">
+                <Button>Go Home</Button>
+            </Link>
+            {/* <PaymentTable /> */}
         </section>
     );
 }
