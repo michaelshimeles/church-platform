045faf34e304377dc9b4efc2fd596a2d395303dc
Michael Shimeles <michaelwasihun96@gmail.com>
2024-02-17 01:52:27 -0500

    Change team images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,49 @@
 import Image from "next/image";
 
 const AboutUs = () => {
-    const team = [{
-        name: "Pastor Berhanu & Pastor Almaz",
-        position: "Senior & Executive Pastor",
-        src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/almiyeandpastor.png`
-    }, {
+    const senior_team = [{
+        name: "Pastor Berhanu",
+        position: "Senior Pastor",
+        src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/pb.png`
+    },
+    {
+        name: "Pastor Almaz",
+        position: "Executive Pastor",
+        src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/hawie.png`
+    },
+    {
         name: "Pastor Simon",
-        position: "Administrative Coordinator",
-        src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/placeholder.png`
-
-    }, {
-        name: "Michael Shimeles",
-        position: "Youth Pastor",
-        src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/mike.png`
+        position: "Pastor",
+        src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/simon.png`
 
     }]
+
+    const team = [
+        {
+            name: "Bizuye Ayenew",
+            position: "Deacon Ministry",
+            src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/bizu.png`
+
+        },
+        {
+            name: "Juliyata Mekonen",
+            position: "Worship Ministry",
+            src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/juliye.png`
+
+        },
+        {
+            name: "Tsega Kebede",
+            position: "Evangelism Ministry",
+            src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/tsegaye.png`
+
+        }
+        , {
+            name: "Michael Shimeles",
+            position: "Youth Pastor",
+            src: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/mike.png`
+
+        }
+    ]
 
     return (
         <section className='container px-6 py-10 mx-auto'>
@@ -32,7 +60,20 @@ const AboutUs = () => {
             </div>
             <div className='mt-[3rem]'>
                 <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                    Our Team</h1>
+                    Our Senior Team</h1>
+                <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4">
+                    {senior_team?.map((member) => (<div key={member?.name} className="flex flex-col items-center p-8 transition-colors duration-300 transform border rounded-xl hover:border-transparent group  dark:border-gray-700 dark:hover:border-transparent">
+                        <Image width={120} height={130} className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src={member?.src} alt="" />
+
+                        <h1 className="mt-4 text-md font-semibold text-gray-700 capitalize dark:text-white">{member?.name}</h1>
+
+                        <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">{member?.position}</p>
+                    </div>))}
+                </div>
+            </div>
+            <div className='mt-[3rem]'>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Our Leadership Team</h1>
                 <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4">
                     {team?.map((member) => (<div key={member?.name} className="flex flex-col items-center p-8 transition-colors duration-300 transform border rounded-xl hover:border-transparent group  dark:border-gray-700 dark:hover:border-transparent">
                         <Image width={120} height={130} className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src={member?.src} alt="" />
