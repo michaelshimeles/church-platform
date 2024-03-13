a8c8c1b149caf9870ab770ce2380cc5e2659d49e
Michael Shimeles <michaelwasihun96@gmail.com>
2024-03-13 15:36:21 -0400

    Testing done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import ServiceSchedule from '@/components/ServiceSchedule'
 import ChurchCard from '@/components/card/ChurchCard'
 import Hero from '@/components/hero/Hero'
-import { googleAuth } from '@/utils/actions/google'
 
 
 export default async function Home() {
-
-  const response = await googleAuth()
-
-  console.log('response', response)
   return (
     <main className="flex min-w-screen flex-col items-center justify-between">
       <Hero />
